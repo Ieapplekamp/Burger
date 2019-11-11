d064fd0ee37a4365f76790dbd621657868945b11
Ian Applekamp <ieapplekamp@gmail.com>
2019-11-11 18:51:18 -0500

    seed is set up

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Turkey Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Mac", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Quarter Pounder W/ Cheese", false);
+
+SELECT * FROM burgers
