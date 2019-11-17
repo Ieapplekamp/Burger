3f6d9a6cd8738fc26dc2fdeb8b03e892a070b176
Ian Applekamp <ieapplekamp@gmail.com>
2019-11-16 20:11:09 -0500

    added the index page. things are connected but not not displaying data

--- config/orm.js
+++ config/orm.js
@@ -1,41 +1,84 @@
 var connection = require("../config/connection.js");
+// Helper function for SQL syntax. If we want to pass 3 values into the MySQL query, we will need 3 question marks. This function will loop throught the data and create an array of question marks and then turn it into a string.
+function printQuestionMarks(num) {
+    var arr = [];
 
-var orm = {
-    all: function(table, cb){
-        let sql = `SELECT * FROM ${table};`;
-        connection.query(sql, function(err, res){
-            if (err) throw err;
-            cb(res);
-        });
-    },
-    specific: function(table, condition, cb){
-        let sql = `SELECT * FROM ${table} WHERE ${condition};`;
-        connection.query(sql, function(err, res){
-            if (err) throw err;
-            cb(res);
-        });
-    },
-    create: function(table, cols, vals, cb){
-        let sql = `INSERT INTO ${table} ( ${cols.join(',')} ) VALUES ( ${vals.map(x => '?').join(',')} );`
-        connection.query(sql, vals, function(err, res){
-            if (err) throw err;
-            cb(res);
-        })
-    },
-    update: function(table, vals, condition, cb){
-        let sql = `UPDATE ${table} SET ${vals} WHERE ${condition};`
-        connection.query(sql, vals, function(err, res){
-            if (err) throw err;
-            cb(res);
-        })
-    },
-    delete: function(table, condition, cb){
-        let sql = `DELETE FROM ${table} WHERE ${condition};`;
-        connection.query(sql, function(err, res){
-            if (err) throw err;
-            cb(res);
-        })
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
     }
+
+    return arr.toString();
 }
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+
+    // Loop through the keys and push the key/value pairs to SQL syntax
+    for (var key in ob) {
+        var value = ob[key];
+
+        // Check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+
+            // If a string with spaces, add quotations
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    // Translate the array of strings to a single comma-separated string
+    return arr.toString();
+}
+
+// Functions for SQL syntax, (1) selectAll, (2) insertOne, (3) updateOne
+var orm = {
+    selectAll: function(input, cb) {
+        var queryString = "SELECT * FROM " + input + ";"
+        connection.query(queryString, function(err, result) {
+            if(err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += " )";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+};
+
 
 module.exports = orm;
