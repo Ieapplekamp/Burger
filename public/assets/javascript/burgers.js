521b4244dcbd47cd55f8097cfbb210c5c4298435
Ian Applekamp <ieapplekamp@gmail.com>
2019-11-17 20:21:43 -0500

    burgers can now be eaten

--- public/assets/javascript/burgers.js
+++ public/assets/javascript/burgers.js
@@ -22,21 +22,20 @@ $(function() {
       );
     });
 
-    $(".change-devour").on("click", function(event) {
+    $(".devoured").on("click", function(event) {
       var id = $(this).data("id");
-      var newDevour = $(this).data("newdevour");
+      var newDevoured = $(this).data("newdevoured");
   
-      var newDevourState = {
-        devoured: newDevour 
+      var newDevouredState = {
+        devoured: newDevoured
       };
   
-      // Send the PUT request.
+
       $.ajax("/api/burgers/" + id, {
         type: "PUT",
-        data: newDevourState
-      }).then(() => {
-        console.log("changed devour to", newDevour);
-        // Reload the page to get the updated list
+        data: newDevouredState
+      }).then(function(){
+        console.log("changed devoured to", newDevoured);
         location.reload();
       });
     });
